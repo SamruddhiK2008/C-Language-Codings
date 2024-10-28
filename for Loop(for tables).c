53006f863f1fd8057cc884a0a20a2583e0aff61f
SamruddhiK2008 <samruddhikakade566@gmail.com>
2024-10-28 21:36:55 +0530

    Create for Loop(for tables).c

---
+++ for Loop(for tables).c
@@ -0,0 +1,28 @@
+//code for FOR LOOP//
+//code for table//
+#include<stdio.h>
+int main ()
+{
+  int m, numbers,ans;
+printf("Enter a number:");
+scanf("%d",&number);
+for(m=1;m,=10;m++)
+{
+ans=number*m;
+printf("\n%d*%d=%d",m number,ans);
+}
+}
+
+output:
+Enter a number:7
+  1*7=7
+  2*7=14
+  3*7=21
+  4*7=28
+  5*7=35
+  6*7=42
+  7*7=49
+  8*7=56
+  9*7=63
+  10*7=70
+  
