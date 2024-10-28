237c266a98299cdc0aade3909fd5ee84391f0950
SamruddhiK2008 <samruddhikakade566@gmail.com>
2024-10-28 21:17:44 +0530

    Create for loop (increasing order).c

---
+++ for loop (increasing order).c
@@ -0,0 +1,25 @@
+//code for FOR LOOP//
+//code for increasing order//
+#include<stdio.h>
+int main()
+{
+  int m;
+for(m=0;m<=10;m++)
+{
+print("\n%d",m);
+}
+}
+
+
+output:
+0
+  1
+  2
+  3
+  4
+  5
+  6
+  7
+  8
+  9
+  10
