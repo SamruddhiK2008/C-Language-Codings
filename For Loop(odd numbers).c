2b11791b4fcdbae6693f2e8d8d2c7132cd8e5bd7
SamruddhiK2008 <samruddhikakade566@gmail.com>
2024-10-28 21:21:54 +0530

    Create For Loop(odd numbers).c

---
+++ For Loop(odd numbers).c
@@ -0,0 +1,33 @@
+//code for FOR LOOP//
+//code for odd numbers//
+#include<stdio.h>
+int main()
+{
+  int m;
+for(m=1;m,=30;m++)
+{
+//odd numbers//
+if(m%2!=0)
+{
+printf("\n%d",m);
+}
+}
+}
+
+
+output:
+1
+  3
+  5
+  7
+  9
+  11
+  13
+  15
+  17
+  19
+  21
+  23
+  25
+  27
+  29
