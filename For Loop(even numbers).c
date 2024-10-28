26acd1e70f20cf2a3da754b8a11093a2073ef71e
SamruddhiK2008 <samruddhikakade566@gmail.com>
2024-10-28 21:29:26 +0530

    Create For Loop(even numbers).c

---
+++ For Loop(even numbers).c
@@ -0,0 +1,35 @@
+//code for  FOR LOOp//
+//code for even numbers//
+#include<sdtdio.h>
+int main()
+{
+  int m;
+for(m=1;m<=30;m++)
+{
+//even numbers//
+if(m%2==0)
+{
+printf("\n%d",m);
+}
+}
+}
+
+
+
+output:
+2
+  4
+6
+  8
+  10
+  12
+  14
+  16
+  18
+  20
+  22
+  24
+  26
+  28
+  30
+  
